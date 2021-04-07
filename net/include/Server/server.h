119dfecd5e9182bf87e2e94ef615d6fc0e5f7de0
Dmitry <dimmarvel@mail.ru>
2021-04-07 21:20:24 +0300

    Add includes. Add class and fields description. Add fields for Server.

--- net/include/Server/server.h
+++ net/include/Server/server.h
@@ -6,12 +6,20 @@
 #define NETWORKING_SERVER_H
 #include <iostream>
 #include <thread>
-#include <unordered_map>
-#include <vector>
-#include <cstring>
-#include <sys/socket.h>
+#include <poi
+#include <memory>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 /**
  * @namespace net
@@ -19,15 +27,30 @@
  */
 namespace net
 {
+    /**
+     * @class Server
+     * @brief My server realisation
+     */
     class Server
     {
-    private:
-        int32_t _port;
-        std::string _address;
     public:
         Server(int32_t port, std::string address);
         ~Server();
         void start();
+    private:
+        int32_t _port;
+        int16_t _backlog; ///< max inbound connections in the queue
+        int sockfd; ///< listen
+        int new_fd; ///< new connect
+        struct addrinfo hints;
+        std::unique_ptr<addrinfo> server;
+        struct sockaddr_storage their_addr; ///< connected address info
+        socklen_t  sin_size;
+        struct sigaction sa;
+        int yes = 1;
+        char s[INET6_ADDRSTRLEN];
+        int rv;
+        std::string _address;
     };
 };
 #endif //NETWORKING_SERVER_H
